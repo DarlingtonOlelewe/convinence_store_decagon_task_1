7dbf4dd9b8a7a806bb8cfc5cd04969c12390688d
DarlingtonOlelewe <darlington.olelewe@decagon.dev>
2022-02-11 11:58:36 +0100

    second commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String [] args){
+        System.out.println("Is this working");
+        System.out.println(2 * 4);
+        System.out.println();
+    }
+}
